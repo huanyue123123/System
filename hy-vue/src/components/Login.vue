f9985c7f82aeb7e40f861beef7be19627580aa00
xin <450299389@qq.com>
2019-11-28 10:24:51 +0800

    李鑫         1.验证码，格式校验

--- hy-vue/src/components/Login.vue
+++ hy-vue/src/components/Login.vue
@@ -1,15 +1,36 @@
 <template>
   <body id="poster">
-    <el-form label-position="left" class = "login-container" hide-required-asterisk
+    <el-form label-position="left" class = "login-container" hide-required-asterisk :model="loginForm" :rules="rules" ref = "loginForm"
              label-width="80px">
-      <el-form-item label="账号">
+      <el-form-item label="账号" prop="username">
         <el-input type = "text" v-model =  "loginForm.username" placeholder="请输入账号" auto-complete="off" minlength = "6"> </el-input>
       </el-form-item>
-      <el-form-item label="密码">
+
+      <el-form-item label="密码" prop="password">
         <el-input type="password" v-model="loginForm.password" placeholder="请输入密码" auto-complete="off" show-password> </el-input>
       </el-form-item>
+      <el-form-item label="验证码" prop="checkCode">
+        <el-row  >
+          <el-col :xs="12" :md="12" :sm="12" :lg="12">
+            <el-input type="text"  v-model="loginForm.checkCode" placeholder="请输入验证码" auto-complete="off" > </el-input>
+          </el-col>
+          <el-col :xs="12" :md="12" :sm="12" :lg="12" >
+            <div class="identifybox">
+              <div @click="refreshCode">
+                <el-image :src="checkCode" style="height:40px"  ></el-image>
+              </div>
+              <el-button @click="refreshCode" type='text' class="textbtn">看不清，换一张</el-button>
+            </div>
+          </el-col>
+
+        </el-row>
+
+      </el-form-item>
+      <el-form-item>
+
+      </el-form-item>
       <el-form-item style="width: 100%">
-        <el-button type="primary" style="width: 100%;background: #505458;border: none" v-on:click="login">登录</el-button>
+        <el-button type="primary" style="width: 100%;background: #505458;border: none" v-on:click="login('loginForm')">登录</el-button>
       </el-form-item>
     </el-form>
   </body>
@@ -19,32 +40,93 @@
 export default {
   name: 'Login',
   data () {
+    var checkUser = (rule,value,callback) => {
+      if (!value) {
+        return callback(new Error('账号不能为空'));
+      }
+      var regUser = /^[a-zA-Z][a-zA-Z0-9]{4,13}[a-zA-Z]$/;
+      if(!regUser.test(value)){
+        return callback(new Error('账号开头结尾必须为字母且长度6-15'));
+      }
+      callback();
+
+    };
+    var checkPwd = (rule,value,callback) => {
+      if (!value) {
+        return callback(new Error('密码不能为空'));
+      }
+      var regPwd = /^[a-zA-Z][a-zA-Z0-9]{5,14}$/;
+      if(!regPwd.test(value)){
+        return callback(new Error('密码以字母开头长度6-15，且不包含特殊符号'));
+      }
+      callback();
+    };
+
     return {
       loginForm: {
         username: '',
-        password: ''
+        password: '',
+        checkCode:''
+      },
+      checkCode:this.$axios.defaults.baseURL + '/createImg',
+      rules: {
+        username:[
+          { validator: checkUser, trigger: 'blur' }
+        ],
+        password:[
+          { validator: checkPwd, trigger: 'blur' }
+        ],
+        checkCode:[
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { min: 4, max: 4, message: '', trigger: 'blur' }
+        ]
       },
       responseResult: []
     }
   },
   methods: {
-    login () {
+    login (formName) {
+      this.$refs[formName].validate((valid) => {
+        console.log(valid + ";");
+        if (valid) {
+          this.$axios
+            .post('/login', {
+              username: this.loginForm.username,
+              password: this.loginForm.password
+            })
+            .then(successResponse => {
+
+              if (successResponse.data.code === 200) {
+                this.$router.replace({path: '/index'})
+              }
+            })
+            .catch(failResponse => {
+            })
+        } else {
+
+          console.log('error submit!!');
+          return false;
+        }
+      });
+
+
+    },
+    refreshCode(){
       this.$axios
-        .post('/login', {
-          username: this.loginForm.username,
-          password: this.loginForm.password
+        .post('/createImg', {
+
         })
         .then(successResponse => {
 
-          if (successResponse.data.code === 200) {
-            this.$router.replace({path: '/index'})
-          }
+          var num=Math.ceil(Math.random()*10);
+          this.checkCode = this.$axios.defaults.baseURL + "/createImg?"+num;
         })
         .catch(failResponse => {
         })
     }
   }
 }
+
 </script>
 <style>
   .login-container {
@@ -65,12 +147,12 @@ export default {
   }
 
   #poster {
-    background:url("../assets/gelei/1.png") no-repeat;
+   /* background:url("../assets/gelei/1.png") no-repeat;
     background-position: center;
     height: 100%;
     width: 100%;
     background-size: cover;
-    position: fixed;
+    position: fixed;*/
   }
   body{
     margin: 0px;
